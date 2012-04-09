ad5969eb673238c7cb7fef2fbf474476a4358faa
Kirill Kanahin <kkanahin@gmail.com>
2012-04-09 22:25:34 +0700

    Create models for files_list

--- files_list/models.py
+++ files_list/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Categories(models.Model):
+    name_category=models.CharField(max_length=15)
+    
+    
+class Files(models.Model):
+    name_file=models.CharField(max_length=30)
+    upload_date=models.DateField()
+    category=models.ForeignKey(Categories)
+
+class Files_content(models.Model):
+    file=models.ForeignKey(Files)
+    file_content=models.TextField()
+
