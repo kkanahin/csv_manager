f42350ef2dcf46b7e0a9b562753c9212e4a446a4
Kirill Kanahin <kkanahin@gmail.com>
2012-04-14 01:22:57 +0700

    Add simple page in files_manager app

--- files_manager/views.py
+++ files_manager/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def f_list(request):
+    return render_to_response('index.html',{'user':request.user})
