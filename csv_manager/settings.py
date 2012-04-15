ee2192a7fb520642004d8fe22f5abe7cac82c796
Kirill Kanahin <kkanahin@gmail.com>
2012-04-16 00:20:29 +0700

    Replace bootstrap files in app

--- csv_manager/settings.py
+++ csv_manager/settings.py
@@ -27,7 +27,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Asia/Omsk'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -62,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_PATH,'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
