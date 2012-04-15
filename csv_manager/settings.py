10e8e5d6a404f18028b4ac7cbd136e079f9d6ffe
Kirill Kanahin <kkanahin@gmail.com>
2012-04-16 00:28:30 +0700

    configure static files

--- csv_manager/settings.py
+++ csv_manager/settings.py
@@ -70,7 +70,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH,'static'),
+    os.path.join(PROJECT_PATH,'files_manager','static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
