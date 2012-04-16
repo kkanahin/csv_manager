420f8190d4fd5b8b45f565600301861832d30276
Kirill Kanahin <kkanahin@peoplethatcancode.com>
2012-04-16 13:50:32 +0700

    add .travis.yml

--- files_manager/views.py
+++ files_manager/views.py
@@ -4,3 +4,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def f_list(request):
     return render_to_response('index.html',{'user':request.user})
+
+@login_required
+def f_upload(request):
+    return render to response('upload.html',{'form':form})
