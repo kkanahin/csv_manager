b14380de3d5f98c3b6c4c50baea829fe0bba545a
Kirill Kanahin <kkanahin@peoplethatcancode.com>
2012-04-23 00:16:05 +0700

    Change file_view function: one query #23

--- files_manager/views.py
+++ files_manager/views.py
@@ -2,7 +2,7 @@ from django.core.context_processors import csrf
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from files_manager.forms import UploadFileForm
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,Http404
 from files_manager.models import Func_var,Function
 from files_manager.file_handling import handle_uploaded_file
 from django.template import RequestContext
@@ -24,25 +24,21 @@ def file_upload(request):
     return render_to_response('upload.html',{'form':form},\
            context_instance=RequestContext(request))
 
-def GetTableValue(id_file):
-    result={}
-    result['table_value']={get_variable.variable:Function.objects.\
-                           filter(variable=get_variable).\
-                           values_list('function',flat=True)
-                           for get_variable in Func_var.objects.\
-                           filter(data=id_file)}
-    result['column']=range(1,max([len(val) for val in result['table_value'].\
-                     values()])+1)
-    return result
-    
-def view_file(request,file_id):
+def file_view(request,file_id):
     template_variables={}
-    template_variables['table_value']={get_variable.variable:Function.objects.\
-                           filter(variable=get_variable).\
-                           values_list('function',flat=True)
-                           for get_variable in Func_var.objects.\
-                           filter(data=file_id)}
-    template_variables['column']=range(1,max([len(val) for val in template_variables['table_value'].\
-                     values()])+1)
+    template_variables['table_value']={}
+    query_result=Function.objects.filter(variable__data=file_id).\
+        values('variable','variable__variable','function').order_by('variable')
+    if not query_result:
+        raise Http404
+    for fetch_raws in query_result:
+       if not fetch_raws['variable'] in template_variables['table_value'].keys():
+           template_variables['table_value'][fetch_raws['variable']]=[]
+           template_variables['table_value'][fetch_raws['variable']].\
+               append(fetch_raws['variable__variable'])
+       template_variables['table_value'][fetch_raws['variable']].\
+           append(fetch_raws['function'])
+    template_variables['column']=range(1,max([len(val)\
+        for val in template_variables['table_value'].values()]))
     return render_to_response('file_view.html',template_variables,\
            context_instance=RequestContext(request))
