ff9e73ed0b38b79e258ed32c0f464c3815c311b4
Kirill Kanahin <kkanahin@peoplethatcancode.com>
2012-04-26 09:17:16 +0700

    modified:   files_manager/tests.py

--- files_manager/tests.py
+++ files_manager/tests.py
@@ -5,12 +5,11 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.test import TestCase,Client
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestMainPage(TestCase):
+    def TestDefaultPage(self):
+        c=Client()
+        response=c.get('/')
+        self.assertEqual(response.status_code,300)
+        print "hello"
