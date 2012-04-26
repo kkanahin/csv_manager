383a52bc630bbb4fcc63283a84f5207d8eb8bff7
Kirill Kanahin <kkanahin@peoplethatcancode.com>
2012-04-26 09:27:09 +0700

    Merge with HEAD  #32

--- files_manager/tests.py
+++ files_manager/tests.py
@@ -7,14 +7,12 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase,Client
 
-<<<<<<< HEAD
 class TestMainPage(TestCase):
     def TestDefaultPage(self):
         c=Client()
         response=c.get('/')
         self.assertEqual(response.status_code,300)
         print "hello"
-=======
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -31,4 +29,3 @@ class MyTest(TestCase):
         c.login(username='admin',password='02111985')
         response=c.get('/upload/')
         self.assertEqual(response.status_code,302)
->>>>>>> 19309855401042df7ae072e6e2bc7c8711f3050b
