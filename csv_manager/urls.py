67cf0fbe031c221beabcd09864df2d6b78447986
Kirill Kanahin <kkanahin@peoplethatcancode.com>
2012-05-02 23:56:50 +0700

    Add graphs #46

--- csv_manager/urls.py
+++ csv_manager/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -13,9 +14,12 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+#    (r'^static/(?P.*)$', 'django.views.static.serve',{'document_root':'settings.MEDIA_ROOT'}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/',include('registration.backends.default.urls')),
-    url(r'^([\w-]+)?$','files_manager.views.file_list',name="file_list"),
+    url(r'^category/([\w-]+)?$','files_manager.views.file_list',name="file_list"),
+    url(r'^$','files_manager.views.file_list',name="file_list"),
     url(r'^upload/$','files_manager.views.file_upload',name="file_upload"),
     url(r'^file_view/(\d+)/$','files_manager.views.file_view',name="file_view"),
 )
+urlpatterns += staticfiles_urlpatterns()
